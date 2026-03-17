9851abd31236c49ea037e97f4b441a1b7e4190fe
Deva <devaydv18@gmail.com>
2026-03-18 00:42:53 +0530

    server.ts

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    const port = config.port || 3000;
+    const port = config.port || 5513;
 
     app.listen(port, "0.0.0.0", () => {
       console.log(`Server running on port ${port}`);
